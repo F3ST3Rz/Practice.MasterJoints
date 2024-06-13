ee47e4458008f360a18ad8abe27fcfc6ff580984
Romi <88428016+F3ST3Rz@users.noreply.github.com>
2024-06-13 23:21:54 +0300

    -

---
+++ Assets/Scripts/Catapulter.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class Catapulter : MonoBehaviour
+{
+    [SerializeField] private SpringJoint _springJoint;
+    [SerializeField] private float _maxSpring;
+    [SerializeField] private float _minSpring;
+    [SerializeField] private Rigidbody _ScoopRigidbody;
+    [SerializeField] private float _delayRecharge;
+    [SerializeField] private Projectile _prefab;
+    [SerializeField] private Transform _pointSpawn;
+
+    private bool _isStarted = false;
+    private Coroutine _coroutine;
+
+    private void Start()
+    {
+        AddProjectile();
+    }
+    private IEnumerator Recharging()
+    {
+        _ScoopRigidbody.WakeUp();
+        _springJoint.spring = _minSpring;
+        yield return new WaitForSeconds(_delayRecharge);
+        AddProjectile();
+        _isStarted = false;
+        _coroutine = null;
+    }
+
+    private void AddProjectile()
+    {
+        Projectile projectile = Instantiate(_prefab);
+        projectile.transform.position = _pointSpawn.position;
+    }
+
+    public void Launch()
+    {
+        if (_isStarted)
+            return;
+
+        _ScoopRigidbody.WakeUp();
+        _springJoint.spring = _maxSpring;
+        _isStarted = true;
+    }
+
+    public void Recharge()
+    {
+        if (_isStarted == false)
+            return;
+
+        if (_coroutine != null)
+            return;
+
+        _coroutine = StartCoroutine(Recharging());
+    }
+}
---
+++ Assets/Scripts/Swinger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class Swinger : MonoBehaviour
+{
+    [SerializeField] private HingeJoint _hingeJoint;
+    [SerializeField] private float _velocity;
+    [SerializeField] private float _delayRocking;
+
+    private Coroutine _coroutine;
+
+    private IEnumerator Rocking()
+    {
+        var wait = new WaitForSeconds(_delayRocking);
+        JointMotor jointMotor = _hingeJoint.motor;
+
+        for (float currentVelocity = _velocity; currentVelocity >= -_velocity; currentVelocity -= _velocity)
+        {
+            jointMotor.targetVelocity = currentVelocity;
+            _hingeJoint.motor = jointMotor;
+            yield return wait;
+        }
+
+        jointMotor.targetVelocity = 0f;
+        _hingeJoint.motor = jointMotor;
+        _coroutine = null;
+    }
+
+    public void OnRocking()
+    {
+        if (_coroutine != null)
+            return;
+
+        _coroutine = StartCoroutine(Rocking());
+    }
+}
